da478e38cc9e7cd73e811d72806d7889bbcd5eee
rodrixone <rodrirodrigezgabriel@gmail.com>
2020-04-10 11:15:04 -0400

    Añadiendo pagina para que interactue con el login

--- componentes/script.js
+++ componentes/script.js
@@ -1,6 +1,6 @@
 function log_in(form){
 		if(form.userid.value == "roger" && form.password.value =="123")
-			location.href = "https://www.google.com/";
+			location.href = "componentes/musica/musica.html";
 		else
 			alert("The username and the password are wrong");
 }
