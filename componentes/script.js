21431a26cee57ff1276fff5da6c71850d978960e
rodrixone <rodrirodrigezgabriel@gmail.com>
2020-04-12 16:28:54 -0400

    Usuarios a traves de vectores de objetos

--- componentes/script.js
+++ componentes/script.js
@@ -1,6 +1,22 @@
+var users = [{
+		userid:"roger",
+		password: "1234"
+	},
+	{
+		userid:"usera",
+		password: "8524"
+	}
+	];
+	
 function log_in(form){
-		if(form.userid.value == "roger" && form.password.value =="123")
-			location.href = "componentes/musica/musica.html";
-		else
-			alert("The username and the password are wrong");
+	var b = true;
+	for(let i =0 ; i < users.length; i++)
+		if(form.userid.value == users[i].userid && form.password.value == users[i].password){
+		location.href = "componentes/musica/musica.html";
+			b = false;
+		}	
+
+	
+	if(b)
+		alert("El ususario o la contraseña son incorrectos");
 }
